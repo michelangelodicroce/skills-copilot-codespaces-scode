e2d173d2ed894041597181b469a7f0a715a9c619
MitchDC <128051343+michelangelodicroce@users.noreply.github.com>
2025-02-17 15:40:49 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,47 +1,50 @@
-// create web server
-
+//create a web server
+//create a web server
 const http = require('http');
 const fs = require('fs');
-const path = require('path');
 const url = require('url');
-const comments = require('./comments.json');
+const path = require('path');
+const comments = require('./comments');
 
+//create a web server
 http.createServer((req, res) => {
-    const { pathname, query } = url.parse(req.url, true);
-    if (pathname === '/api/comments' && req.method === 'GET') {
-        res.writeHead(200, {
-            'Content-Type': 'application/json'
-        });
-        res.end(JSON.stringify(comments));
-    } else if (pathname === '/api/comments' && req.method === 'POST') {
-        let data = '';
-        req.on('data', (chunk) => {
-            data += chunk;
-        });
-        req.on('end', () => {
-            const comment = JSON.parse(data);
-            comments.push(comment);
-            const newLocal = 'comments.json';
-            fs.writeFile(path.join(__dirname, newLocal), JSON.stringify(comments), 'utf8', (err) => {
-                if (err) {
-                    res.writeHead(500, {
-                        'Content-Type': 'text/plain'
-                    });
-                    res.end('Server Error');
-                    return;
-                }
-                res.writeHead(200, {
-                    'Content-Type': 'application/json'
+    //parse the request
+    const parsedUrl = url.parse(req.url, true);
+    //get the path name
+    let pathname = `.${parsedUrl.pathname}`;
+    //get the method
+    const method = req.method;
+    //get the id from the query string
+    const id = parsedUrl.query.id;
+
+    //if the path name is /comments
+    if (pathname === './comments') {
+        //if the method is GET
+        if (method === 'GET') {
+            //send the comments
+            res.end(JSON.stringify(comments));
+        }
+        //if the method is POST
+        else if (method === 'POST') {
+            //create a variable to store the data
+            let body = '';
+            //set the request encoding to utf-8
+            req.setEncoding('utf-8');
+            //listen for the data event
+            req.on('data', (chunk) => {
+                                //add the chunk to the body
+                                body += chunk;
+                            });
+                
+                            //listen for the end event
+                            req.on('end', () => {
+                                //parse the body as JSON
+                                const newComment = JSON.parse(body);
+                                //add the new comment to the comments array
+                                comments.push(newComment);
+                                //send the new comment
+                                res.end(JSON.stringify(newComment));
+                            });
+                        }
+                    }
                 });
-                res.end(JSON.stringify(comment));
-            });
-        });
-    } else {
-        res.writeHead(404, {
-            'Content-Type': 'text/plain'
-        });
-        res.end('Not Found');
-    }
-}).listen(3000, () => {
-    console.log('Server running at http://localhost:3000/');
-});
